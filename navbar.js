a52f9b096ed1d92c60a809617f48baeadb3394c3
matteoxx <mateuszniziolek96@gmail.com>
2019-03-22 15:52:50 +0100

    kolokwium poprawiony

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 let nav = '<h1>Nawigacja</h1>  <a href="index.html">Home</a>' +
     '<a href="kontakt.html">Kontakt</a>';
 
-let elem = document.querySelector(".navbar");
+let elem = document.querySelector(".nav-bar");
 elem.innerHTML = nav;
